5eb6e9b69416077c6618c474c3cefd64972ea84d
root <root@localhost.localdomain>
2020-04-29 11:45:57 +0300

    update 29042020

--- mp3.sh
+++ mp3.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 #Обновление youtube
 youtube-dl -U
+#Копируем плейл лист в директорию скрипта
 scp -P 22222 admin@10.0.0.2:/share/CACHEDEV1_DATA/VM/youtube/YouTube/playlist_mp3.txt /home/Sync/mp3_script/playlist.txt
+# устанавливаем переменные
 date="$(date +%d%m%Y)"
-cp /home/Sync/mp3_script/history.txt /home/Sync/mp3_script/history_$date.txt
-cp /home/Sync/mp3_script/playlist.txt /home/Sync/mp3_script/playlist_$date.txt
 dir=/home/Sync/mp3_script
 mix=/share/CACHEDEV1_DATA/Music/Mix
 sborka=/share/CACHEDEV1_DATA/Music/Sborka
 pl=/share/CACHEDEV1_DATA/VM/youtube/YouTube
-
-echo "Создана копия истории и плейлиста"
 
 #переменные для закачки
 you="--extract-audio"
@@ -21,6 +19,13 @@ pl="playlist.txt"
 hs="history.txt"
 rs="result.txt"
 tm="temp.txt"
+
+
+# Создаем копию истории и плейлиста
+cp /home/Sync/mp3_script/history.txt /home/Sync/mp3_script/history_$date.txt
+cp /home/Sync/mp3_script/playlist.txt /home/Sync/mp3_script/playlist_$date.txt
+echo "Создана копия истории и плейлиста"
+
 
 #находим уникальные записи
 comm -23 <(sort $pl) <(sort $hs)|cat > $rs
@@ -39,6 +44,7 @@ cat $rs >> $hs
 echo "История дописанна"
 :>$rs
 echo "result очищен"
+
 #удалить дубликаты в истории
 uniq <(sort $hs) > $tm
 mv $tm $hs
@@ -47,5 +53,18 @@ echo "создан новый файл истории"
 echo "Копируем файлы на Qnap"
 find $dir -name '*.mp3' -size +102400k -exec scp -P 22222 '{}' admin@10.0.0.2:$mix \;
 find $dir -name '*.mp3' -size -30720k -exec scp -P 22222 '{}' admin@10.0.0.2:$sborka \;
+
+#Загружаем пустой список на Qnap
+echo "Пустой лист отправили на Qnap"
 scp -P 22222 /home/Sync/mp3_script/playlist.txt admin@10.0.0.2:/share/CACHEDEV1_DATA/VM/youtube/YouTube/playlist_mp3.txt
+
+#Удаляем все mp3 записи
 find /home/Sync/mp3_script -name '*.mp3' -exec rm -f {} \;
+echo "Файлы mp3 удалены"
+
+
+
+
+
+
+
